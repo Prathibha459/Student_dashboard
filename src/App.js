ee24f75775640fd92fbae1cbac28354bc78821a1
sreeja-333 <bezawadaprathibhasreeja@gmail.com>
2024-06-30 21:11:21 +0530

    added functionality to student in navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Student from './pages/student';
+import { Switch } from '@headlessui/react';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/students" element={<Student />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
